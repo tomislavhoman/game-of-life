966db676e511cb9dc849d2be7191b76b9e52cd87
Tomislav Homan <tomislav.homan@fiveminutes.eu>
2016-06-14 20:06:13 +0200

    Add more examples

--- app/src/engine.rs
+++ app/src/engine.rs
@@ -6,10 +6,15 @@ pub struct Engine {
 
 impl Engine {
 	pub fn run(&self) -> Board {
-		let row1 = vec![true, true, false, true];
-		let row2 = vec![true, false, false, true];
-		let row3 = vec![false, true, true, true];
+		let row1 = vec![true, true, false, true, true, false, true, true];
+		let row2 = vec![true, false, false, true, false, true, true, false];
+		let row3 = vec![false, true, true, true, true, false, false, true];
+		let row4 = vec![false, true, true, false, true, false, false, true];
+		let row5 = vec![false, true, true, true, true, false, false, true];
+		let row6 = vec![false, false, true, true, true, false, false, true];
+		let row7 = vec![false, true, false, false, true, false, false, true];
+		let row8 = vec![false, false, true, true, false, false, false, true];
 
-		return Board {rows: vec![row1, row2, row3]};
+		return Board {rows: vec![row1, row2, row3, row4, row5, row6, row7, row8]};
 	}
 }
